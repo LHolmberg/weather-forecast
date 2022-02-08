a3e95d98fbe53b6092fe017591fdec906ddae960
LHolmberg <lukazlovicc@hotmail.com>
2022-02-08 21:05:50 +0100

    build

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -23,7 +23,7 @@ class WeatherHelper extends React.Component {
 
     componentDidMount() {
         fetch(
-            "https://api.openweathermap.org/geo/1.0/direct?q="+this.props.city.replace(/ /g, '_') +"&limit=5&appid=ebd7ba44716d363e4cf939b5c8a73e46/")
+            "https://api.openweathermap.org/geo/1.0/direct?q="+this.props.city.replace(/ /g, '_') +"&limit=5&appid=ebd7ba44716d363e4cf939b5c8a73e46")
             .then((geoRes) => geoRes.json())
             .then((geoJson) => {
                 fetch(
